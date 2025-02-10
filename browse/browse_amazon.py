82e011e5b95ed6d85906c93b88ada91ebfc10afa
Leon <laixin@evecom.net>
2025-02-10 22:09:32 +0800

    optimize: optimize browsing amazon product

--- browse/browse_amazon.py
+++ browse/browse_amazon.py
@@ -62,7 +62,33 @@ class AmazonBrowser:
         self.chrome.close_window()
         time.sleep(1)
         self.chrome.quit_browser()
-        print("quit")
+        print("窗口已退出")
+
+
+def iter_windows(ws, url):
+    for w in ws:
+        browser_window = AmazonBrowser(w["id"])
+        print(f"{w['name']} 正在打开，浏览:{url}")
+        # 根据keyword浏览
+        # browser_window.browse_by_keyword(keyword)
+        # 根据网址直接浏览
+        browser_window.browse_direct(url)
+        print(f"{w['name']} 浏览完成！")
+        browser_window.quit_browser()
+
 
 if __name__ == '__main__':
- pass
+    group = bit_api.get_group_by_name("测试")
+    gid = group['id']
+    keyword = "Snake Eye Tactical Two ToneFinish Fantasy Desgin NinjaSword Comes with " \
+              "Nylon Sheath(Red)"
+    url = "https://www.amazon.com/Snake-Eye-Tactical-Martial-Throwing/dp/B07DP5PCZ6"
+
+    index = 0
+    while True:
+        windows = get_window_by_gid(gid, index, 50)
+        if windows:
+            iter_windows(windows["list"], url)
+        if index == windows["totalNum"] - 1:
+            break
+        index += 1
--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-from browse import browse_amazon as window
 
-gid = '2c9bc061948ca74b0194d9dd9c002a18'
-keyword = "Snake Eye Tactical Two ToneFinish Fantasy Desgin NinjaSword Comes with " \
-          "Nylon Sheath(Red)"
-url = "https://www.amazon.com/Snake-Eye-Tactical-Martial-Throwing/dp/B07DP5PCZ6"
-
-
-def iter_windows(ws):
-    for w in ws:
-        browser_window = window.AmazonBrowser(w["id"])
-        # 根据keyword浏览
-        # browser_window.browse_by_keyword(keyword)
-        # 根据网址直接浏览
-        browser_window.browse_direct(url)
-
-
-for i in range(1000):
-    windows = window.get_window_by_gid(gid, i, 10)
-    if windows:
-        print(windows)
-        iter_windows(windows["list"])
-    if i == windows["totalNum"] - 1:
-        break
+print("Hello World")
+# nothing here
